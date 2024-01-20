3c3f84169f0dc53d4401e0a90c8b4da0cbc94723
yf <youfeng_android@163.com>
2024-01-20 17:44:50 +0800

    1.增加AOP

---
+++ startup/src/main/java/com/cms/startup/annotation/AgreeProtocol.kt
@@ -0,0 +1,10 @@
+package com.cms.startup.annotation
+
+/**
+ * AUTHOR：YF
+ * DATE: 2024/1/19 0019
+ * DESCRIBE:
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AgreeProtocol
---
+++ startup/src/main/java/com/cms/startup/aop/AgreeAspectJ.kt
@@ -0,0 +1,9 @@
+package com.cms.startup.aop
+
+/**
+ * AUTHOR：YF
+ * DATE: 2024/1/19 0019
+ * DESCRIBE:
+ */
+class AgreeAspectJ {
+}
